78a47f26a6febee29f9015e76fffa7093fc36b1d
Maxime AUBE <maxime.aube@protonmail.com>
2022-01-03 01:58:26 +0100

    début dev pour prise en charge multi-serveur

---
+++ events/guildDelete.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'guildDelete',
+    execute (guild, client) {
+        console.log("Left a guild: " + guild.name);
+        //console.log(client.guilds.cache);
+    }
+};
